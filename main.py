9051a568e13195ba796bae957d0c40f218321c47
mohalaam <120230216+mohalaam@users.noreply.github.com>
2024-11-13 22:21:28 +0100

    main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ app, rt = fast_app()
 
 @rt("/")
 def get():
-    return Div(P("{
-  "Description": "Fichier de vérification de la propriété du domaine pour Microsoft 365 : placer dans la racine du site web",
-  "Domain": "dsgsdgsdgsd.up.railway.app",
-  "Id": "f58a1e05-6512-4491-9510-7b16670249fe"
-}"), hx_get="/change")
+    return Div(
+        P(r'{
+            "Description": "Fichier de vérification de la propriété du domaine pour Microsoft 365 : placer dans la racine du site web",
+            "Domain": "dsgsdgsdgsd.up.railway.app",
+            "Id": "f58a1e05-6512-4491-9510-7b16670249fe"
+        }'), hx_get="/change")
 
 
 serve()
